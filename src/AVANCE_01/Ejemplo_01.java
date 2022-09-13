6cac40d2ed684c78947762297e9d2ea57177c99d
LuisNol <nolberto.sumaran@gmail.com>
2022-09-12 21:28:36 -0500

    12/9/2022

--- src/AVANCE_01/Ejemplo_01.java
+++ src/AVANCE_01/Ejemplo_01.java
@@ -13,6 +13,32 @@ package AVANCE_01;
  * @personal_mail nolberto.sumaran@gmail.com
 
  */
-public class Ejemplo_01 {
+class primo {
+
+    public void esprimo(int n) {
+        int cont = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                cont++;
+            }
+        }
+        if (cont == 2) {
+            System.out.print("," + n);
+        }
+    }
+}
+
+public class Ejemplo_01{
+
+    public static void main(String[] args) {
+        primo p1 = new primo();
+        long inicio = System.currentTimeMillis();
+        for (int i = 0; i < 1000000; i++) {
+            p1.esprimo(i);
+        }
+        long fin = System.currentTimeMillis();
+        System.out.println("\n\nTIEMPO DE EJECUCION: " + (fin - inicio));
+    }
 
 }
+
